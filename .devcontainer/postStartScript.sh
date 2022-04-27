84f023ebf97bd2fd475542baaae0c3a6a09e3c21
Erik Whitesides <eklevjer@gmail.com>
2022-04-27 17:50:55 +0000

    rm starship

--- .devcontainer/postStartScript.sh
+++ .devcontainer/postStartScript.sh
@@ -3,11 +3,3 @@
 #make logging folder and chown to vscode user
 PULP_LOGDIR=$(dirname $PULP_LOGPATH)
 sudo mkdir -p $PULP_LOGDIR && sudo chown vscode:vscode $PULP_LOGDIR
-
-#vscode resets .bashrc to their default at container start,
-#and then we append the following to switch to starship
-#starship is installed via Dockerfile
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-
-#copy starship config
-mkdir -p ~/.config && cp ./.devcontainer/starship.toml ~/.config/starship.toml
