01138cb8a058f3ea6310792c745000e059d7caad
Erik Whitesides <eklevjer@gmail.com>
2022-04-28 02:29:17 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 setup config
 """
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="pulp_operations",
@@ -10,7 +10,8 @@ setup(
     author_email="eklevjer@gmail.com",
     description="tool to perform various tasks on pulp server",
     url="https://github.com/ewhitesides/pulp_operations",
-    packages=find_packages(),
+    package_dir={'': 'source'},
+    packages=['pulp_operations'],
     install_requires=[
         "pulpcore-client==3.12.2",
         "pulp-rpm-client==3.11.1",
